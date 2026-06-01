2b2f274c70dc8b5786e27b806dd58f56f9fb6568
APurnawan <155307712+APurnawan@users.noreply.github.com>
2026-06-01 18:52:36 +0700

    Update app.py

--- app.py
+++ app.py
@@ -52,6 +52,8 @@ comparison = st.sidebar.multiselect(
 
 df = load_stock(periode)
 
+st.write(df.columns)
+
 #INDIKATOR
 df["MA20"] = df["Close"].rolling(20).mean()
 df["MA50"] = df["Close"].rolling(50).mean()
